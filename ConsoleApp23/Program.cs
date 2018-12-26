5d178adbd1ef4d751b5f5c512fb5cc178b0dec28
camalzade_elvin <camalzade_elvin@outlook.com>
2018-12-26 21:37:32 +0400

    done pr maybe

--- ConsoleApp23/Program.cs
+++ ConsoleApp23/Program.cs
@@ -1,16 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace ConsoleApp23
+namespace ConsoleApp21
 {
+    interface IObserver
+    {
+        void Update(string info);
+    }
+    class FileLoggerObserver : IObserver
+    {
+        public void Update(string info)
+        {
+            Console.WriteLine($"{info} for File Logger");//file a yaz
+        }
+    }
+    class MailSenderObserver : IObserver
+    {
+        public void Update(string info)
+        {
+            Console.WriteLine($"{info} for Mail Sender");//camaatin mailine gonder
+        }
+    }
+    class Observable
+    {
+        List<IObserver> observers = new List<IObserver>();
+        public void AddObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+        public bool RemoveObserver(IObserver observer)
+        {
+            return observers.Remove(observer);
+        }
+        public void Notify()
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(" . . . Some Information . . . ");
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Observable observable = new Observable();
+            FileLoggerObserver observer = new FileLoggerObserver();
+            MailSenderObserver observer2 = new MailSenderObserver();
+            observable.AddObserver(observer); observable.AddObserver(observer2);
+            observable.Notify();
 
         }
     }
 }
+
+
+
+
